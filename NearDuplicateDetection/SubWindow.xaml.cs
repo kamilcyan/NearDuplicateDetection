cd96eda7d0c4bbc1add71ae07251aabca30488e6
kamilcyan <kamil.lucjanek@wp.pl>
2022-06-23 14:58:15 +0200

    Working with knn, tryin to connect it with FeatureExtraction, working with reducedHash

---
+++ NearDuplicateDetection/SubWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NearDuplicateDetection
+{
+    public partial class SubWindow : Window
+    {
+        public SubWindow()
+        {
+            //InitializeComponent();
+        }
+
+        //public void Show(List<string> results)
+        //{
+        //    ResultList2.TextWrapping = TextWrapping.Wrap;
+        //    foreach (var r in results)
+        //    {
+        //        ResultList2.Text += r;
+        //    }
+        //}
+    }
+}
